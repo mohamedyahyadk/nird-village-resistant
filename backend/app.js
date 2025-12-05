27414fa4010a4121559a7052985b7b5911fecb31
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2025-12-05 02:37:29 -1100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 // app.js
+const path = require("path");
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -15,19 +17,28 @@ require("./config/passport")(passport);
 app.use(passport.initialize());
 
 // Middleware
-app.use(
-  cors({
-    origin: "*", //  Frontend
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*", //  Frontend
+//     credentials: true,
+//   })
+// );
+app.use(cors());
+
 // app.use(bodyParser.json());
 
 // Routes
 app.use("/auth", authRoutes);
 app.use("/api", secureRoutes);
-app.get("/", (req, res) => {
-  res.send("🚀 NIRD Backend Server is running!");
+// app.get("/", (req, res) => {
+//   res.send("🚀 NIRD Backend Server is running!");
+// });
+
+// Serve React frontend
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 // Start server
