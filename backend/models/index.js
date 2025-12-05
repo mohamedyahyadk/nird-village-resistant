5c83e3f8e0124c3d99c530cde1a5cecc01fa9057
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2025-12-05 02:13:48 -1100

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,15 +1,17 @@
 "use strict";
+require("dotenv").config();
+
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.url) {
+  sequelize = new Sequelize(config.url, config);
 } else {
   sequelize = new Sequelize(
     config.database,
@@ -20,10 +22,11 @@ if (config.use_env_variable) {
 }
 
 fs.readdirSync(__dirname)
-  .filter(
-    (file) =>
+  .filter((file) => {
+    return (
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-  )
+    );
+  })
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(
       sequelize,
