1a10231333e125d5e99f4f5d9bd692c086676617
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2025-12-05 00:46:19 -1100

    feat(frontend pages)

---
+++ frontend/src/pages/Challenges.js
@@ -0,0 +1,9 @@
+function Resources() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Resources Page</h2>
+    </div>
+  );
+}
+
+export default Resources;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Bienvenue sur NIRD</h1>
+      <p>Platforme pour réduire la dépendance numérique dans les écoles.</p>
+    </div>
+  );
+}
+
+export default Home;
