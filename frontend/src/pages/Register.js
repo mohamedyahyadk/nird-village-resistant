f1574e5e6c53db77b274a1a9464398da8a167d4d
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2025-12-05 02:23:52 -1100

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { API_URL } from "../api";
+// import { API_URL } from "../api";
+import { API_URL } from "../config";
 
 function Register() {
   const [name, setName] = useState("");
